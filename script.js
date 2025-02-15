96afb17fc66ff59df1e0ce456709b3ec6ee9f934
zooch-btw <526sdipalma@frhsd.com>
2025-02-15 20:13:31 +0000

    main info done (big weight off shoulders lol and trial and error with colors as well

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //Quotes Generator
 var quotesArray = [
-    'NAPS is by far the most significant cultural force of the decade. — New York Times',
-    'These days sleeping at your desk is not just acceptable, it’s encouraged. — The Atlantic',
-    'NAPS has changed the way people think about sleep, on a grand scale. — Washington Post',
-    'Happiness increased 200% since NAPS began promoting siestas. — National Science Review',
-    'NAPS has ushered in a new era of sleep. — USA Today',
-    'Not surprisingly, more siestas means more productivity. — Wall Street Journal',
-    'NAPS is doing the hard work of helping people get the rest they need. — U.S. News'
+  "Honesty is the first chapter in the book of wisdom. — Thomas Jefferson",
+  "I like the dreams of the future better than the history of the past. — Thomas Jefferson",
+  "The tree of liberty must be refreshed from time to time with the blood of patriots and tyrants — Thomas Jefferson",
+  "In matters of style, swim with the current; in matters of principle, stand like a rock.  — Thomas Jefferson",
+  "Do you want to know who you are? Don’t ask. Act! Action will delineate and define you. — Thomas Jefferson",
+  "When the people fear the government, there is tyranny; when the government fears the people, there is liberty. — Thomas Jefferson",
+  "Nothing can stop the man with the right mental attitude from achieving his goal; nothing on earth can help the man with the wrong mental attitude. — Thomas Jefferson",
 ];
 var randomNumber = Math.floor(Math.random() * quotesArray.length);
-document.getElementById("press-quote").textContent = '“' + quotesArray[randomNumber].split('—')[0] + '” — ' +
-    quotesArray[randomNumber].split('—')[1];
+document.getElementById("press-quote").textContent =
+  "“" +
+  quotesArray[randomNumber].split("—")[0] +
+  "” — " +
+  quotesArray[randomNumber].split("—")[1];
 //console.log(quotesArray[randomNumber].split('—')[0])
